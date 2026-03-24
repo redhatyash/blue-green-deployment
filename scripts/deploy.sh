072c87fb009aa7e0d759f94ba038b39ab17f094d
redhatyash <saiy8077@gmail.com>
2026-03-24 23:26:35 +0530

    Modified workflow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,9 +7,10 @@ COLOR=$2
 echo "Deploying $COLOR with image $IMAGE_TAG"
 
 if [ "$COLOR" == "green" ]; then
-  sed "s|IMAGE_PLACEHOLDER|myorg/myapp:$IMAGE_TAG|g" k8s/green.yaml | kubectl apply -f -
+  sed "s|IMAGE_PLACEHOLDER|saiy8077/myapp:$IMAGE_TAG|g" k8s/green-deployment.yaml | kubectl apply -f -
+  kubectl apply -f k8s/green.yml
 else
-  kubectl apply -f k8s/blue.yaml
+  kubectl apply -f k8s/blue-deployment.yaml
 fi
-
+kubectl apply -f k8s/service.yaml
 kubectl rollout status deployment/app-$COLOR
